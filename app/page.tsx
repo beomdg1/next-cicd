614d61485477b924c8dc4d7a5b3da1e6cbe743a0
beomdg1 <nguyenhoan08092017@gmail.com>
2026-03-28 15:47:17 +0700

    update2

--- app/page.tsx
+++ app/page.tsx
@@ -6,5 +6,8 @@ export default function Page() {
   return <>
       <h1>App Router</h1>
       <p>Beo context is nope</p>
+      <p>Beo ne 123</p>
+      <p>Beo ne 123</p>
+      <p>Beo ne 123</p><p>Beo ne 123</p>
   </>;
 }
